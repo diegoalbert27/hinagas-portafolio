248c32bcf53476e68978bf2b8bc13b491f52a963
Diego Hinagas <hinagasrodriguez@gmail.com>
2023-07-09 22:32:53 -0400

    fix: base updated

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/'
-  }
-
-  if (command !== 'serve') {
-    config.base = '/hinagas-portafolio/'
-  }
-
-  return config
+export default defineConfig({
+  plugins: [react()],
+  base: 'https://diegoalbert27.github.io/hinagas-portafolio/'
 })
