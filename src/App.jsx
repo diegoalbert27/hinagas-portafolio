ffc308a5b45674be629543d74cc6aeca653dc1df
Diego Hinagas <hinagasrodriguez@gmail.com>
2023-07-24 13:06:30 -0400

    feat: created footer component

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import { Navbar } from "./components/Navbar"
 import { Header } from "./components/Header"
 import { About } from "./components/About"
 import { Portfolio } from "./components/Portfolio"
-
-
+import { Footer } from "./components/Footer"
 
 export default function App() {
   return (
@@ -12,10 +11,7 @@ export default function App() {
       <Header />
       <About />
       <Portfolio />
-
-      <footer className="text-center bg-slate-900 text-stone-100 py-4 font-light italic">
-        Hecho con amor &#128147;, deveras! - 2023
-      </footer>
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer className="text-center bg-slate-900 text-stone-100 py-4 font-light italic">
+      Hecho con amor &#128147;, deveras! - 2023
+    </footer>
+  )
+}
