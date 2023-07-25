e6ef5295581bcd5c1a76a388e772aec07fdd683f
Diego Hinagas <hinagasrodriguez@gmail.com>
2023-07-24 21:06:52 -0400

    fix: slide effect removed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,7 @@
-import { Slide } from "react-awesome-reveal";
-
 export function Footer() {
   return (
     <footer className="text-center bg-slate-900 text-stone-100 py-4 font-light italic">
-      <Slide direction="up">
-        Hecho con amor &#128147; - 2023
-      </Slide>
+      Hecho con amor &#128147; - 2023
     </footer>
   )
 }
