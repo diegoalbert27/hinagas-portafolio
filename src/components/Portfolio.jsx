339a265e3af97ec4486e44262e96474c58bab46e
Diego Hinagas <hinagasrodriguez@gmail.com>
2023-10-20 16:29:54 -0400

    fix: changed links in projects

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,6 +1,4 @@
 import bpc from "../assets/images/cards/bpc.png"
-// import card2 from "../assets/images/cards/2.jpg"
-// import card3 from "../assets/images/cards/3.jpg"
 
 export function Portfolio() {
   return (
@@ -12,7 +10,7 @@ export function Portfolio() {
       <div className="flex flex-col md:flex-row md:justify-center gap-4 p-8 md:p-2 md:mt-5">
         <div className="bg-stone-900 text-stone-100 rounded-md md:w-56 shadow-lg">
           <div className="p-3">
-            <a href="https://github.com/diegoalbert27/bpc_v2Vani" target="_blank" rel="noreferrer">
+            <a href="http://bpcaragua.com/" target="_blank" rel="noreferrer">
               <img
                 className="w-full h-28 rounded-md shadow-md"
                 src={bpc}
@@ -54,7 +52,7 @@ export function Portfolio() {
 
         <div className="bg-stone-900 text-stone-100 rounded-md md:w-56 shadow-lg">
           <div className="p-3">
-            <a href="https://github.com/diegoalbert27/simple-online-store" target="_blank" rel="noreferrer">
+            <a href="https://simple-online-store-production.up.railway.app" target="_blank" rel="noreferrer">
               <img
                 className="w-full h-28 rounded-md shadow-md"
                 src="https://user-images.githubusercontent.com/48531350/272437822-fbe5fbe9-3d7a-4a45-b882-dc0e596e04a0.png"
@@ -95,39 +93,6 @@ export function Portfolio() {
             </div>
           </div>
         </div>
-
-        {/* <div className="bg-stone-900 text-stone-100 rounded-md md:w-56 shadow-lg">
-          <div className="p-3">
-            <img
-              className="w-full h-28 rounded-md shadow-md"
-              src={card3}
-              alt=""
-            />
-
-            <h3 className="text-md font-sans font-semibold mt-2">
-              Lorem ipsum dolor sit amet.
-            </h3>
-            <p className="text-sm mt-2 font-sans text-stone-400">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro,
-              est!
-            </p>
-
-            <div className="flex flex-wrap gap-1 mt-2">
-              <div className="bg-stone-100 text-stone-950 rounded-full px-2 py-1">
-                <p className="text-xs font-sans">HTML</p>
-              </div>
-              <div className="bg-stone-100 text-stone-950 rounded-full px-2 py-1">
-                <p className="text-xs font-sans">CSS</p>
-              </div>
-              <div className="bg-stone-100 text-stone-950 rounded-full px-2 py-1">
-                <p className="text-xs font-sans">Javascript</p>
-              </div>
-              <div className="bg-stone-100 text-stone-950 rounded-full px-2 py-1">
-                <p className="text-xs font-sans">Web</p>
-              </div>
-            </div>
-          </div>
-        </div> */}
       </div>
     </div>
   )
